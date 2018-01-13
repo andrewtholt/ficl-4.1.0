062132bdd10acab383ccfd5c2fe2c97bd7cb4b4f
Andrew Holt (On Laptop) <andrewtholt60@gmail.com>
2018-01-13 11:40:41 +0000

    Added get message size to helper

--- HelperLibs/MQTT/callbacks.c
+++ HelperLibs/MQTT/callbacks.c
@@ -1,4 +1,16 @@
 #include <mosquitto.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+struct cbMqttMessage {
+    uint8_t msgFlag;
+    char topic[64];
+    char payload[32];
+} ;
+
+uint32_t sizeOfMessage() {
+    return ( sizeof( struct cbMqttMessage ));
+}
 
 void messageCallback(struct mosquitto *mosq, void *obj,const struct mosquitto_message *message) { 
 }
