b32fc7f2810a548a900b0fbca2c360959d20abb7
Andrew Holt <andrewtholt60@gmail.com>
2018-01-19 11:38:57 +0000

    Improved install scripts

--- HelperLibs/MQTT/install.sh
+++ HelperLibs/MQTT/install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+echo "Moving libmqttcallback.so"
 sudo mv libmqttcallback.so /usr/local/lib/
 sudo ldconfig
+
+FICL_DIR="/usr/local/lib/ficl"
+sudo mkdir -p $FICL_DIR
+echo "Copying mqtt.fth"
+sudo cp ./mqtt.fth $FICL_DIR
--- HelperLibs/Utils/install.sh
+++ HelperLibs/Utils/install.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+echo "Moving libutils.so"
 sudo mv libutils.so /usr/local/lib
 sudo ldconfig
+
+echo "Copying utils.fth"
+FICL_DIR="/usr/local/lib/ficl"
+sudo mkdir -p $FICL_DIR
+sudo cp ./utils.fth $FICL_DIR
